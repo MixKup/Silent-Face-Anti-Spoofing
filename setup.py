6751332235543f767912eae8871a4276b3280683
Mix_Kup <dub404@outlook.com>
2022-07-19 11:49:06 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('requirements.txt') as f:
     required = f.read().splitlines()
     
 setup(
-    name='Silent-Face-Anti-Spoofing',
+    name='mixkup_antispoofing',
     packages=find_packages(),
     url='https://github.com/MixKup/Silent-Face-Anti-Spoofing.git',
     description='This is a description for antispoof',
